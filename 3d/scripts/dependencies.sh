f58e76c63128067da36e4ee4fca21a72fec421ab
Scott Bezek <scottbez1@gmail.com>
2016-08-10 01:02:01 -0700

    try installing some other mesa packages that were previously mentioned in passing builds

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -4,5 +4,5 @@ set -ev
 
 sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb libgl1-mesa-dev libgl1-mesa-glx libglapi-mesa mesa-common-dev
 
