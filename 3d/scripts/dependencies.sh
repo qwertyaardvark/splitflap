c24259963442707381fb0d08acdee4818ba24334
Scott Bezek <scottbez1@gmail.com>
2016-08-17 00:04:42 -0700

    Print some more debugging info 2

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -5,4 +5,4 @@ set -ev
 #sudo add-apt-repository --yes ppa:openscad/releases
 #sudo apt-get update -qq
 #openscad
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgl1-mesa-glx libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy libglapi-mesa
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgl1-mesa-glx libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy mesa-utils
