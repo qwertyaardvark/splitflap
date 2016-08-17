8fb94a25d1fa832f75c3d2e10e71fe9c696f47ed
Scott Bezek <scottbez1@gmail.com>
2016-08-17 09:29:04 -0700

    Remove fglrx

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -6,3 +6,4 @@ set -ev
 #sudo apt-get update -qq
 #openscad
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgl1-mesa-glx libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy mesa-utils
+sudo apt-get purge -y fglrx
