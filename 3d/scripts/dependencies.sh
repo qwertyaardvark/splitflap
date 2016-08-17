af73fdba2cb4bc1856dc5e6a7c071f78632e75ba
Scott Bezek <scottbez1@gmail.com>
2016-08-16 22:55:55 -0700

    More hax to see if I can get xvfb to work on travis

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -4,6 +4,5 @@ set -ev
 
 sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy
-sudo update-alternatives --display gl_conf
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgl1-mesa-glx libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy
 sudo dpkg-reconfigure xserver-xorg
