57af329cbc2d059a204ded13524b2bb7618a93d4
Scott Bezek <scottbez1@gmail.com>
2016-08-17 09:34:14 -0700

    Revert most travis changes except keep purging fglrx

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -2,8 +2,7 @@
 
 set -ev
 
-#sudo add-apt-repository --yes ppa:openscad/releases
-#sudo apt-get update -qq
-#openscad
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y inkscape imagemagick xvfb libglib2.0-dev libgl1-mesa-dri libgl1-mesa-glx libgles2-mesa-dev freeglut3-dev libfreetype6-dev xorg-dev libssl-dev libbz2-dev xserver-xorg-input-void xserver-xorg-video-dummy mesa-utils
+sudo add-apt-repository --yes ppa:openscad/releases
+sudo apt-get update -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb
 sudo apt-get purge -y fglrx
